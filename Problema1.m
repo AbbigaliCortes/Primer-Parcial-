a1c20bb0be30453245643d910951797bd3e8c065
AbbigaliCortes <43357503+AbbigaliCortes@users.noreply.github.com>
2018-10-03 17:26:36 -0500

    Add files via upload

---
+++ Problema1.m
@@ -0,0 +1,10 @@
+disp('Veremos si el numero introducido es par o inpar')
+n=input ('Introdusca el numero a estudiar: ');
+while n>1
+n=n-2;
+end
+if n==1
+disp('el numero es impar')
+else
+disp('el numero es par')
+end
---
+++ problema2.m
@@ -0,0 +1,8 @@
+disp('Verifiquemos si las calificaciones son mayor a 70')
+n=input ('Introdusca la calificacion a verificar:' );
+
+if (n>=70)
+    disp('Felicidades has aprovado')
+else 
+    disp('Continua con tus estudias necesitas esforzarte mas')
+end
