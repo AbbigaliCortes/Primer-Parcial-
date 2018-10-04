9373e8ab8cccc1028ffb2e56742fdd14027b3da5
AbbigaliCortes <43357503+AbbigaliCortes@users.noreply.github.com>
2018-10-03 19:24:51 -0500

    Add files via upload

---
+++ problema6.m
@@ -0,0 +1,16 @@
+prompt=('litros consumidos')
+n=input(prompt)
+
+if n<=50
+    cuota=150
+    if n>50
+        if n<200
+            cuota= ((1500)+(n-50)*3)
+        end
+    end
+end
+if n>200
+    cuota=(600)+(n-200)*5
+end
+            
+         
